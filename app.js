144fd619a092a1d3a28115e3e4b9c346adc4f73a
alProkopov <prokopov.vrn@gmail.com>
2021-09-04 20:16:39 +0300

    add and delete task functions

--- app.js
+++ app.js
@@ -1,90 +1,82 @@
-const tasks = [
-    {
-        _id: '5d2ca9e2e03d40b326596aa7',
-        completed: true,
-        body:
-            'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
-        title: 'Eu ea incididunt sunt consectetur fugiat non.',
-    },
-    {
-        _id: '5d2ca9e29c8a94095c1288e0',
-        completed: false,
-        body:
-            'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
-        title:
-            'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
-    },
-    {
-        _id: '5d2ca9e2e03d40b3232496aa7',
-        completed: true,
-        body:
-            'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
-        title: 'Eu ea incididunt sunt consectetur fugiat non.',
-    },
-    {
-        _id: '5d2ca9e29c8a94095564788e0',
-        completed: false,
-        body:
-            'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
-        title:
-            'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
-    },
-];
+//объект объектов(хранилище тасков)
+let objectOfTask = {}
+console.log(objectOfTask)
 
-(function (arrOfTasks) {
+//form
+const form = document.forms['addTask'];
+const inputTitle = form.elements['title'];
+const inputBody = form.elements['body'];
+//deleteButton
+const deleteButtonParent = document.querySelector('.container .list-group')
+console.log(deleteButtonParent)
 
-    const objOfTasks = arrOfTasks.reduce((acc, task) => {
-        acc[task._id] = task;
-        return acc
-    }, {});
+//событие submit для формы
+form.addEventListener('submit', ValueHandler);
+//событие удаления
+deleteButtonParent.addEventListener('click', deleteHandler);
 
-    //Ui
-    const listContainer = document.querySelector('.tasks-list-section .list-group')
-    console.log(listContainer)
-
-    renderAllTasks(objOfTasks);
-
-    function renderAllTasks(tasksList) {
-        if (!tasksList) {
-            console.error('Передайте список задач');
-            return;
-        }
-
-        const fragment = document.createDocumentFragment();
-        Object.values(tasksList).forEach(task => {
-            const li = listItemTemplate(task)
-            fragment.appendChild(li)
-        });
-
-        listContainer.appendChild(fragment)
+function deleteHandler (e) {
+    if (e.target.classList.contains('delete-btn')) {
+        let parent = e.target.closest('[data-task-id]')
+        let id = parent.dataset.taskId;
+        delete objectOfTask[id]
+        parent.remove()
+        console.log( id)
+        console.log(' hello ')
     }
+  
+}
 
-    function listItemTemplate({_id, title, body} = {}) {
-        const li = document.createElement('li');
-        li.classList.add(
-            'list-group-item',
-            'd-flex',
-            'align-items-center',
-            'flex-wrap',
-            'mt-2');
-        const span = document.createElement('span');
-        span.textContent = title;
-        span.style.fontWeight = 'bold';
+// обработчик
+function ValueHandler (e) {
+    e.preventDefault();
+    const titleValue = inputTitle.value
+    const bodyValue = inputBody.value
 
-        const deleteBtn = document.createElement('button');
-        deleteBtn.textContent = 'Delete task'
-        deleteBtn.classList.add('btn', 'btn-danger', 'ml-auto', 'delete-btn');
+    const newTaskValue = createTask(titleValue,bodyValue);
 
-        const article = document.createElement('p')
-        article.textContent = body;
-        article.classList.add('mt-2', 'w-100');
+    const newListElement = createLiElement(newTaskValue)
 
-        li.appendChild(span);
-        li.appendChild(deleteBtn);
-        li.appendChild(article);
+    document.querySelector('.list-group').insertAdjacentElement('afterbegin', newListElement )
+    form.reset()
+}
 
-        return li
+
+//создание одной задачи
+function createTask(title, body) {
+    const newTask = {
+        title,
+        body,
+        _id: `task-${Math.random()}`
     }
+    objectOfTask[newTask._id] = newTask;
+    return newTask
 
-})(tasks);
+}
+
+//создание одного элемента списка
+function createLiElement ({_id, title, body} = {}) {
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'd-flex', 'align-items-center', 'flex-wrap', 'mt-2')
+    li.setAttribute('data-task-id', _id);
+
+    const span = document.createElement('span');
+    span.textContent = title;
+    span.classList.add ('bold');
+
+    const paragraph = document.createElement('p')
+    paragraph.textContent = body;
+    paragraph.classList.add ('mt-2', 'w-100');
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.classList.add ('btn', 'btn-danger', 'ml-auto', 'delete-btn');
+
+    li.appendChild(span);
+    li.appendChild(deleteButton);
+    li.appendChild(paragraph);
+
+
+    return li
+}
 
